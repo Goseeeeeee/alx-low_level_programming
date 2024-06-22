29cafbb92bf6ddfaab30be0e8a2d3f42fd22cc83
Gosee <gosee.gena@gmail.com>
2024-06-22 16:43:49 -0700

    a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - a prog that prt all sing dig number base 10 fro 0
+ *
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	printf("0123456789\n");
+
+	return (0);
+}
