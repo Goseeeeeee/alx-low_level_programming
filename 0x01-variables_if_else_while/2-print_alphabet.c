386a2b9edd6f2bcd919cc0830e53335171d2937a
Gosee <gosee.gena@gmail.com>
2024-06-22 15:06:07 -0700

    task 3: using for loop to print alpha putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabet in lowercase.
+ *
+ * Return: Always zero
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		putchar(ch);
+
+	return (0);
+}
