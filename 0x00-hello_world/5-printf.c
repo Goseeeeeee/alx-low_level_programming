8b13ed41f0e3beeb74b12589974dacee56385927
Gosee <gosee.gena@gmail.com>
2024-06-21 17:42:42 -0700

    before last

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a funtion that prints
+ *
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
