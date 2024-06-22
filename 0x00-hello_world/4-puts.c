6432e08194a9b32f877e4f36c525f5eaa1ebdd56
Gosee <gosee.gena@gmail.com>
2024-06-21 17:17:07 -0700

    first prog

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is just a main function
+ *
+ * Return: always returns zero
+ */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return(0);
+}
